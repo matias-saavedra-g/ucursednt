e59ad926b626fe6caa477a69d8797fd21cb1010a
Matias Ignacio Saavedra Guerra <38592663+matias-saavedra-g@users.noreply.github.com>
2024-08-12 17:22:27 -0400

    v3.3
    Enhances easter egg with video and transitions

--- js/easterEgg.js
+++ js/easterEgg.js
@@ -7,5 +7,27 @@ newLi.innerHTML = '<a href="#" class="opcion"><span class="glyphicon glyphicon-t
 ulElement.appendChild(newLi);
 
 newLi.addEventListener('click', () => {
-  document.documentElement.innerHTML = ''; // Removes all content
+  // Plays this video when the user clicks on the new LI https://www.youtube.com/watch?v=4DxBkW3r8pA&pp=ygUldGhpcyBpcyBteSBmaW5hbCBtZXNzYWdlIGdvb2RieWUgbWVtZQ%3D%3D
+  const video = document.createElement
+  ('iframe');
+  video.src = 'https://www.youtube.com/embed/4DxBkW3r8pA?autoplay=1';
+  video.width = '560';
+  video.height = '315';
+  video.frameborder = '0';
+  video.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+  video.allowfullscreen = true;
+  document.body.appendChild(video);
+
+  // Waits for the video to finish and then removes it
+  video.addEventListener('ended', () => {
+    video.remove();
+  });
+  
+  // Fades the opacity of entire document to 0 in 5 s
+  document.documentElement.style.transition = 'opacity 5s';
+  document.documentElement.style.opacity = 0;
+  setTimeout(() => {
+    // Removes all content after 5 s
+    document.documentElement.innerHTML = '';
+  }, 5000);
 });
